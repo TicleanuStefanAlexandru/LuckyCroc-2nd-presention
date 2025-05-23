f67ff509e10024ea79c8abc5532fb588a906068c
TicleanuStefanAlexandru <ticleanu.stefan.s7a@student.ucv.ro>
2025-05-23 16:26:02 +0300

    Add files via upload

---
+++ CASINO/ExitScript.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitZoneScript : MonoBehaviour
+{
+    public GameObject popupPanel; // Assign this in Inspector
+    public Text popupText;        // Assign this in Inspector
+    public Button exitButton;     // Assign this in Inspector
+    public string targetScene;    // Name of the scene to load (e.g., "Dave's Apartment")
+
+    void Start()
+    {
+        popupPanel.SetActive(false);
+
+        if (exitButton != null)
+            exitButton.onClick.AddListener(ExitToScene);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popupText.text = "Do you want to end your gambling session and go home?";
+            popupPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popupPanel.SetActive(false);
+        }
+    }
+
+    void ExitToScene()
+    {
+        if (!string.IsNullOrEmpty(targetScene))
+        {
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+}
